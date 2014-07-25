962760bad2320a4d6b182bbf69ea1ceb1c8175bf
Aaron Dufour <aarond@fogcreek.com>
2014-07-25 14:14:23 -0400

    Use a better internal representation of the strings
    Now we use the position of the character instead of the character
    itself.  This means that calculating the hash is way faster (no
    memory lookups in lettersReversed) and incrementing a character
    is faster (straight increment instead of a letters/lettersReversed
    lookup).  When we've found the right answer, we do the conversion
    to the right string (one lookup into letters per character).
    
    We stopped generating lettersReversed since its not longer being
    used.
    
    This might be considered cheating, given our initial goal,
    which is why I did it last.
    
    Time is now 0.23 seconds.

--- hash.c
+++ hash.c
@@ -3,40 +3,40 @@
 #include <stdio.h>
 #include <time.h>
 
-#define ITERATION_COUNT 5
+#define ITERATION_COUNT 20
 
 static char* letters = "acdegilmnoprstuw";
 static int lettersLength = 16;
-static char* lettersReversed;
 
 void setup() {
-  lettersReversed = calloc(sizeof(int), 0xFF);
-  for(int i = 0;i < lettersLength;i++)
-    lettersReversed[letters[i]] = i;
 }
 
 static long hash(char* str, int length) {
   long h = 7;
   for(int i = 0;i < length;i++) {
-    h = h * 37 + lettersReversed[str[i]];
+    h = h * 37 + str[i];
   }
   return h;
 }
 
 static char* unhash(long target, int length) {
   char* str = calloc(sizeof(char), length + 1);
-  memset(str, letters[0], sizeof(char) * length);
+  memset(str, 0, sizeof(char) * length);
   str[length] = '\0';
   while(1) {
     int i = 0;
-    str[i] = letters[lettersReversed[str[i]] + 1];
-    while(str[i] == '\0') {
-      str[i] = letters[0];
+    str[i]++;
+    while(str[i] == lettersLength) {
+      str[i] = 0;
       i++;
-      str[i] = letters[lettersReversed[str[i]] + 1];
+      str[i]++;
     }
-    if(hash(str, length) == target)
+    if(hash(str, length) == target) {
+      for(int j = 0;j < length;j++) {
+        str[j] = letters[str[j]];
+      }
       return str;
+    }
   }
 }
 
@@ -44,7 +44,8 @@ int main() {
   setup();
 
   // Does hash work?  Should be 680131659347.
-  long hashed = hash("leepadg", 7);
+  // Input is now "\x06\x03\x03\x0a\x00\x02\x04"
+  long hashed = hash("\x06\x03\x03\x0a\x00\x02\x04", 7);
   if(hashed != 680131659347L)
     printf("%ld should be 680131659347\n", hashed);
   else
